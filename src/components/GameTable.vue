4b50b9e9190498a781034aeaff334ae52e6233fd
m_mingareev <amingaraev@smartdatatech.io>
2022-12-11 21:37:19 +0300

    first

---
+++ src/components/GameTable.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="game-table">
+        <p>kwnfewlkn</p>
+    </div>
+</template>
+<style lang="scss">
+    .game-table {
+        border: 1px solid red
+    }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <GameTable/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//  import HelloWorld from '@/components/HelloWorld.vue'
+import GameTable from '@/components/GameTable.vue'
+export default {
+  name: 'Home',
+  components: {
+    // HelloWorld,
+    GameTable
+  }
+}
+</script>
